dca061aa6c7c90ad5fae401fd4891c0473da6175
CrhistoferVera <cristofervera110@gmail.com>
2026-01-09 11:25:49 -0400

    anadir variables de entorno

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,8 +3,8 @@ import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { sepolia, bscTestnet } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
-    appName: 'HUNBOLI',
-    projectId: '693f4df90fbe52ab9283a245264a369c', // <--- PEGA TU ID AQUÍ
+    appName: process.env.PROJECT_NAME || 'HUNBOLI',
+    projectId: process.env.PROJECT_ID || '693f4df90fbe52ab9283a245264a369c', // <--- PEGA TU ID AQUÍ
     chains: [sepolia, bscTestnet], // Las redes que soportas
     ssr: true, // Server Side Rendering (Importante para Next.js)
 });
