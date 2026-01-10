71286e62ad5615b61fadd809e9b15642d927b118
Jhaseft <jhasesaat@gmail.com>
2026-01-10 11:49:31 -0400

    refactor: rename icon components for clarity and consistency

--- src/components/dashboard/ui/calendar.tsx
+++ src/components/dashboard/ui/calendar.tsx
@@ -60,13 +60,14 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
+        IconPrevious: ({ className, ...props }) => (
           <ChevronLeft className={cn("size-4", className)} {...props} />
         ),
-        IconRight: ({ className, ...props }) => (
+        IconNext: ({ className, ...props }) => (
           <ChevronRight className={cn("size-4", className)} {...props} />
         ),
       }}
+
       {...props}
     />
   );
