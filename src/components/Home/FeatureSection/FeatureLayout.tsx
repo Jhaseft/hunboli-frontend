c2b7601150711121f78276b668666e0773552b41
Jhaseft <jhasesaat@gmail.com>
2026-01-10 11:07:17 -0400

    refactor: update FeatureLayout component to use flexbox layout and improve type annotations

--- src/components/Home/FeatureSection/FeatureLayout.tsx
+++ src/components/Home/FeatureSection/FeatureLayout.tsx
@@ -1,15 +1,17 @@
-export function FeatureLayout({ children, reverse = false }) {
+import { ReactNode } from "react";
+
+interface FeatureLayoutProps {
+  children: [ReactNode, ReactNode];
+  reverse?: boolean;
+}
+
+export function FeatureLayout({ children, reverse = false }: FeatureLayoutProps) {
   const [first, second] = children;
 
   return (
-    <div className="grid md:grid-cols-2  lg:grid-cols-2 gap-12 sm:gap-16 md:gap-20 items-center mb-20 sm:mb-24 md:mb-32">
-      <div className={reverse ? "order-1 lg:order-2 sm:order-2" : "order-1"}>
-        {first}
-      </div>
-
-      <div className={reverse ? "order-2 lg:order-1 sm:order-1" : "order-2"}>
-        {second}
-      </div>
+    <div className={`flex ${reverse ? "flex-row-reverse" : "flex-row"}`}>
+      {first}
+      {second}
     </div>
   );
 }
