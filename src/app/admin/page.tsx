6112b548604cff3d485edbbb8b08c2917bf21042
CrhistoferVera <cristofervera110@gmail.com>
2026-01-29 16:34:22 -0400

    panel del admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,18 @@
+import { DashboardHeader } from "@/components/admin/DashboardHeader";
+import { StatsCards } from "@/components/admin/StatsCards";
+import { KycAlertBanner } from "@/components/admin/KycAlertBanner";
+import { MintRequestsSection } from "@/components/admin/MintRequestsSection";
 
-
-export default function page() {
-    return (
-        <div>page del admin</div>
-    )
+export default function AdminDashboardPage() {
+  return (
+    <div className="max-w-7xl mx-auto flex flex-col gap-6">
+      <DashboardHeader
+        title="Panel de Control"
+        subtitle="Gestión de reservas, emisiones y redenciones"
+      />
+      <StatsCards />
+      <KycAlertBanner pendingCount={3} totalDeposits="45,000 Bs" />
+      <MintRequestsSection />
+    </div>
+  );
 }
