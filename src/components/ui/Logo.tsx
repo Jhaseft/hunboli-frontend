703d04d64218b45bc8a221146b11fe85e6243524
CrhistoferVera <cristofervera110@gmail.com>
2026-02-09 15:24:46 -0400

    Delete verify-account route

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,12 +1,17 @@
 import Link from 'next/link';
 
-export function Logo() {
+interface LogoProps {
+  width?: number;
+  height?: number;
+}
+
+export function Logo({ width, height }: LogoProps) {
   return (
     <Link href="/" className="flex justify-center">
       <img
         src="https://res.cloudinary.com/dnbklbswg/image/upload/v1767889917/apple-icon-removebg-preview_cjptux.png"
         alt="Hunboli"
-        className="w-24 h-24 object-contain rounded-full "
+        className={`w-${width || 24} h-${height || 24} object-contain rounded-full`}
       />
     </Link>
   );
